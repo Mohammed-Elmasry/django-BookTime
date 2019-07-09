b3bbea4bba20f6b15921721fb8eed4ebdacaca86
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-10 00:19:29 +0200

    rectify the html code in contact_form.html

--- main/forms.py
+++ main/forms.py
@@ -8,6 +8,7 @@ class ContactForm(forms.Form):
     name = forms.CharField(label='Your name', max_length=100)
     message = forms.CharField(max_length=600, widget=forms.Textarea)
 
+    # this defines a wrapper function...
     def send_mail(self):
         logger.info("Sending email to customer service")
         message = "From: {0}\n{1}".format(
