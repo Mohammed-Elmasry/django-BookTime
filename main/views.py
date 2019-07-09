3a9f272d6d5ac4e74e3bbbfd62a148634c7d4128
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-09 23:46:11 +0200

    define a class-based view inheriting from FormView to render the contact_us form

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic.edit import FormView
+from main import forms
 
-# Create your views here.
+
+# our first class-based view definition
+
+class ContactUsView(FormView):
+    template_name = "contact_us.html"
+    form_class = forms.ContactForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
