dd455d41c362cecb3d5c8f4d26b01783b94dda76
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-12 01:39:28 +0200

    define the make_title method inside the newly created empty migration

--- main/migrations/0007_productname_titlize.py
+++ main/migrations/0007_productname_titlize.py
@@ -5,9 +5,19 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
+    def make_title(apps, schema):
+        Product = apps.get_model('main','Product')
+        for product in Product.objects.all():
+            product.name = product.name.title()
+            product.save()
+
     dependencies = [
         ('main', '0006_productname_capitalize'),
     ]
 
     operations = [
+        migrations.RunPython(
+            make_title,
+            migrations.RunPython.noop
+        )
     ]
