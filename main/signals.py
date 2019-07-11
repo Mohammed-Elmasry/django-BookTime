e630b9d3ccc08589496352d19b40704ac29a9872
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-11 21:12:30 +0200

    add explanatory comments

--- main/signals.py
+++ main/signals.py
@@ -15,6 +15,7 @@ def generate_thumbnail(sender, instance, **kwargs):
         "Generating thumbnail for product %d",
         instance.product.id
     )
+    # image processing logic
     image = Image.open(instance.image)
     image = image.convert("RGB")
     image.thumbnail(THUMBNAIL_SIZE, image.ANTIALIAS)
