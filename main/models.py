8a87f7ecad515c46027242ea63f62fa96054ceec
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-12 00:21:35 +0200

    add active boolean field to Product model

--- main/models.py
+++ main/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
     description = models.TextField(blank = True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     slug = models.SlugField(max_length=48)
+    active = models.BooleanField(default = True)
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
     objects = ActiveManager()
