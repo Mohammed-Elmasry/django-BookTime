fc6090d3d5eac08dbc250898041c4dfae890c894
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-13 19:56:37 +0200

    customize the admin view for Product model

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug", "in_stock", "price"]
+    list_filter = ["active", "in_stock", "date_updated"]
+    list_editable = ["in_stock"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug" : ("name", )}
+
+
+admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.ProductTag)
 admin.site.register(models.ProductImage)
