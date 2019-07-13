a642bc43840c61ce4d29e0b905f17858b32a3a8c
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-13 20:09:21 +0200

    define admin classes for ProductTag and ProductImage models

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from . import models
 
 # Register your models here.
@@ -11,6 +12,27 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug" : ("name", )}
 
 
+class ProductTagAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    list_filter = ["active"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug" : ("name",)}
+    autocomplete_fields = ("product",)
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_filter = ["thumbnail_tag", "product_name"]
+    readonly_fields = ["thumbnail"]
+    search_fields = ["product_name"]
+
+    def thumbnail_tag(self, obj):
+        if obj.thumbnail:
+            return format_html('<img src="%s"/>' % obj.thumbnail.url)
+        return "-"
+    thumbnail_tag.short_description = "Thumbnail"
+
+    def product_name(self, obj):
+        return obj.product.name
+
 admin.site.register(models.Product, ProductAdmin)
-admin.site.register(models.ProductTag)
-admin.site.register(models.ProductImage)
+admin.site.register(models.ProductTag, ProductTagAdmin)
+admin.site.register(models.ProductImage, ProductImageAdmin)
