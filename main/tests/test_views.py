87162012c0c3797aa2de99e2bc0174589e254a48
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-15 11:45:14 +0200

    define automated test function to check for the Products_list page

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
+from decimal import Decimal
+from main import models
 
 # Create your tests here.
 class TestPage(TestCase):
@@ -15,3 +17,33 @@ class TestPage(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'about_us.html')
         self.assertContains(response, "BookTime")
+
+
+    def test_products_page_returns_active(self):
+        models.Product.objects.create(
+            name="The cathedral and the bazaar",
+            slug="cathedral-bazaar",
+            price=Decimal("10.00")
+        )
+
+        models.Product.objects.create(
+            name = "A tale of two cities",
+            slug = "tale-two-cities",
+            prce = Decimal("2.00"),
+            active = False
+        )
+
+        response = self.client.get(
+            reverse("products", kwargs={"tag":"all"})
+        )
+
+        # testing if the response returned successfully and base template extension works
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "BookTime")
+
+        # testing if the products um expecting to come back actually came back in response
+        product_list = models.Product.objects.active().order_by("name")
+        self.assertEqual(
+            list(response.context["object_list"]),
+            list(product_list)
+        )
