1b6f907966fd6f20429569933e1ad06de1cb6b76
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-15 11:07:42 +0200

    define the ListView for the products list page

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.edit import FormView
+from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404
 from main import forms
-
+from main import models
 
 # our first class-based view definition
 
@@ -12,3 +14,26 @@ class ContactUsView(FormView):
     def form_valid(self, form):
         form.send_mail()
         return super().form_valid(form) # after validation, render success url
+
+
+# our second class-based view definition
+
+
+class ProductListView(ListView):
+    template_name = "main/product_list.html"
+    paginate_by = 4
+
+    def get_queryset(self):
+        print("Moody here's your stuff : ", self.kwargs)
+        tag = self.kwargs["tag"]
+        self.tag = None
+        if tag != 'all':
+            self.tag = get_object_or_404(
+                models.ProductTag, slug=tag
+            )
+        if self.tag:
+            product = models.Product.objects.active().filter(tags = self.tag)
+        else:
+            products = models.objects.active()
+        return products.order_by("name")
+
