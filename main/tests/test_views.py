7dcc2cb2acd88794d955adfc69d52ad72132733f
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-07-15 12:30:43 +0200

    define the test case for the tag filter functionality

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -34,7 +34,7 @@ class TestPage(TestCase):
         )
 
         response = self.client.get(
-            reverse("products", kwargs={"tag":"all"})
+            reverse("products", kwargs={"tag": "all"})
         )
 
         # testing if the response returned successfully and base template extension works
@@ -43,7 +43,33 @@ class TestPage(TestCase):
 
         # testing if the products um expecting to come back actually came back in response
         product_list = models.Product.objects.active().order_by("name")
+
         self.assertEqual(
             list(response.context["object_list"]),
             list(product_list)
         )
+
+    def test_products_page_filter_by_tags_active(self):
+        cb = models.Product.objects.create(
+            name = "The cathedral and the bazaar",
+            slug="cathedral-bazaar",
+            price = Decimal("10.00")
+        )
+
+        cb.tags.create(name = "Open source", slug="opensource")
+        models.Product.objects.create(
+            name = "Microsoft windows guide",
+            slug="microsoft-windows-guide",
+            price=Decimal("12.00")
+        )
+
+        response = self.client.get(
+            reverse("products", kwargs={"tag": "opensource"})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "BookTime")
+        product_list = (models.Product.objects.active().filter(tags__slug="opensource")
+                        .order_by("name"))
+
+        self.assertEqual(list(response.context["object_list"]),
+                         list(product_list))
